448bc65cae338efeb589c6358f060c86cb06764f
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-06-05 09:03:22 -0400

    Entity user e Rest funcionando

---
+++ src/main/java/br/com/mfsdevsystem/workshopmongo/domain/Post.java
@@ -0,0 +1,10 @@
+package br.com.mfsdevsystem.workshopmongo.domain;
+
+import java.io.Serializable;
+
+public class Post  implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+
+}
