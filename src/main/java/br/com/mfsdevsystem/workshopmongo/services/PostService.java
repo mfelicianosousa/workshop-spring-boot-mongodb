a034a369e9ed7254ece8fc656ae8b1847c74f9e1
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-06-07 11:13:18 -0400

    Obtendo um post por id

---
+++ src/main/java/br/com/mfsdevsystem/workshopmongo/services/PostService.java
@@ -0,0 +1,33 @@
+package br.com.mfsdevsystem.workshopmongo.services;
+
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.mfsdevsystem.workshopmongo.domain.Post;
+import br.com.mfsdevsystem.workshopmongo.repository.PostRepository;
+import br.com.mfsdevsystem.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository postRepository;
+	
+	public Post findById(String id ) {
+
+		try {
+	    	Post post = postRepository.findById( id ).get();
+			
+		   return post;
+		} catch (NoSuchElementException e) {
+			throw new ObjectNotFoundException("Object não encontrado!");
+		}
+		
+	}
+
+	
+	
+	
+}
