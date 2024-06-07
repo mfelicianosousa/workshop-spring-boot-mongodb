39794d325035b548dc145aa790f1c582828c10e6
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-06-07 08:17:44 -0400

    Referenciando os posts do usuário

--- src/main/java/br/com/mfsdevsystem/workshopmongo/domain/User.java
+++ src/main/java/br/com/mfsdevsystem/workshopmongo/domain/User.java
@@ -1,9 +1,12 @@
 package br.com.mfsdevsystem.workshopmongo.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "user")
@@ -18,11 +21,10 @@ public class User implements Serializable {
 	private String password;
 	private boolean isAdmin;
 
-	/*
-	 * @JsonIgnore
-	 * 
-	 * @OneToMany(mappedBy = "post") private List<Post> posts = new ArrayList<>();
-	 */
+
+	@DBRef(lazy=true)
+    private List<Post> posts = new ArrayList<>();
+    
 	public User() {
 
 	}
@@ -72,10 +74,16 @@ public class User implements Serializable {
 	public void setAdmin(boolean isAdmin) {
 		this.isAdmin = isAdmin;
 	}
+	
+	public List<Post> getPosts() {
+		return posts;
+	}
 
-	/*
-	 * public List<Post> getPosts() { return posts; }
-	 */
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -93,4 +101,5 @@ public class User implements Serializable {
 		return Objects.equals(id, other.id);
 	}
 
+	
 }
