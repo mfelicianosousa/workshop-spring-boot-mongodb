b9740644470d6a5b1abb385eef584f499f0c9b8f
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-06-07 17:06:45 -0400

    Consulta simples com @Query

--- src/main/java/br/com/mfsdevsystem/workshopmongo/repository/PostRepository.java
+++ src/main/java/br/com/mfsdevsystem/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package br.com.mfsdevsystem.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.mfsdevsystem.workshopmongo.domain.Post;
@@ -10,6 +11,9 @@ import br.com.mfsdevsystem.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
 
+	@Query("{'Title': {$regex: ?0, $options: 'i'} }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 	
 	
--- src/main/java/br/com/mfsdevsystem/workshopmongo/services/PostService.java
+++ src/main/java/br/com/mfsdevsystem/workshopmongo/services/PostService.java
@@ -29,7 +29,8 @@ public class PostService {
 	}
 
 	public List<Post> findByTitle(String text){
-		return postRepository.findByTitleContainingIgnoreCase( text );
+		// return postRepository.findByTitleContainingIgnoreCase( text );
+		return postRepository.searchTitle( text );
 	}
 	
 	
