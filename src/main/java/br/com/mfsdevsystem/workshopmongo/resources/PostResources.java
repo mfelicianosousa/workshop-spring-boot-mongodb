368e4e0607faa86ceed3215f171fcf824901b17e
Marcelino Feliciano de Sousa <marcelino.feliciano@hotmail.com>
2024-06-07 18:50:25 -0400

    Consulta com vários criterios

--- src/main/java/br/com/mfsdevsystem/workshopmongo/resources/PostResources.java
+++ src/main/java/br/com/mfsdevsystem/workshopmongo/resources/PostResources.java
@@ -1,5 +1,6 @@
 package br.com.mfsdevsystem.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,21 @@ public class PostResources {
 		return ResponseEntity.ok().body( listPost );
 	}
 	
+	@RequestMapping(value = "/full-search", method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(value="text", defaultValue="") String text,
+			@RequestParam(value="text", defaultValue="") String minDate,
+			@RequestParam(value="text", defaultValue="") String maxDate){
+		
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		
+		
+		List<Post> listPost = postService.fullSearch(text, min, max);
+
+		return ResponseEntity.ok().body( listPost );
+	}
 
 }
 
--- src/main/java/br/com/mfsdevsystem/workshopmongo/services/PostService.java
+++ src/main/java/br/com/mfsdevsystem/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package br.com.mfsdevsystem.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -33,6 +34,10 @@ public class PostService {
 		return postRepository.searchTitle( text );
 	}
 	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
+	}
 	
 	
 }
